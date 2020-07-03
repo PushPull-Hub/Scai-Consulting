d21570e76068f9ab99dd0c12575f462cd561a475
Dhn-Coding <60256254+Dhn-Coding@users.noreply.github.com>
2020-07-03 02:58:38 +0200

    improve the app-demo styles

--- social-network-app/src/app/components/create-post/create-post.component.ts
+++ social-network-app/src/app/components/create-post/create-post.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.css']
+  styleUrls: ['./create-post.component.scss'],
 })
 export class CreatePostComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
