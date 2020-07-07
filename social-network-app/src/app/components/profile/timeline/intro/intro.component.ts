a01c5e6d5d21b2488caa5f7302f97fc5e8071152
Dhn-Coding <60256254+Dhn-Coding@users.noreply.github.com>
2020-07-07 15:58:35 +0200

    ability of changing the user informations

--- social-network-app/src/app/components/profile/timeline/intro/intro.component.ts
+++ social-network-app/src/app/components/profile/timeline/intro/intro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserServices } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-intro',
@@ -6,7 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent implements OnInit {
-  constructor() {}
+  EditButtonClicked: boolean = false;
+  loggedUser = this.userService.loggedUser;
+  usersList = this.userService.getUsers();
+  location: string = '';
+  hometown: string = '';
+  work_in: string = '';
+  birthday: string = '';
+  constructor(private userService: UserServices) {}
 
   ngOnInit(): void {}
+
+  showInputs = () => {
+    this.EditButtonClicked = true;
+  };
+  editUserInfos = (f: NgForm) => {
+    const location = f.value.location;
+    const hometown = f.value.hometown;
+    const work_in = f.value.work_in;
+    const birthday = f.value.birthday;
+
+    this.location = location;
+    this.hometown = hometown;
+    this.work_in = work_in;
+    this.birthday = birthday;
+
+    this.userService.updateUser(
+      this.userService.loggedUserId,
+      'location',
+      location
+    );
+    this.userService.updateUser(
+      this.userService.loggedUserId,
+      'hometown',
+      hometown
+    );
+    this.userService.updateUser(
+      this.userService.loggedUserId,
+      'work_in',
+      work_in
+    );
+    this.userService.updateUser(
+      this.userService.loggedUserId,
+      'birthday',
+      birthday
+    );
+
+    this.EditButtonClicked = false;
+    console.log(this.loggedUser);
+  };
 }
