03644d3c3f4a4bcb7925d6e8dbd4afd42475fd8a
Hamza Salvagno <hamza.salvagno@scai.intra>
2020-07-09 15:52:09 +0200

    generate the 2nd module (HomeWrapper Module)

--- social-network-app/src/app/components/home-wrapper/home-wrapper-routing.module.ts
+++ social-network-app/src/app/components/home-wrapper/home-wrapper-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeWrapperComponent } from './home-wrapper.component';
+import { HomePageComponent } from './home-page/home.component';
 
-const routes: Routes = [{ path: '', component: HomeWrapperComponent }];
+const routes: Routes = [{ path: '', component: HomePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- social-network-app/src/app/components/home-wrapper/home-wrapper.module.ts
+++ social-network-app/src/app/components/home-wrapper/home-wrapper.module.ts
@@ -4,21 +4,21 @@ import { CommonModule } from '@angular/common';
 import { HomeWrapperRoutingModule } from './home-wrapper-routing.module';
 
 import { HomepageModule } from './home-page/homepage.module';
-import { ProfileModule } from './profile/profile.module';
+// import { ProfileModule } from './profile/profile.module';
 // import { HomePageComponent } from './home-page/home.component';
-import { ProfileComponent } from './profile/profile.component';
+// import { ProfileComponent } from './profile/profile.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeWrapperComponent } from './home-wrapper.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeWrapperComponent],
   imports: [
     CommonModule,
     HomeWrapperRoutingModule,
-    HomepageModule,
-    ProfileModule,
     SharedModule,
+    HomepageModule,
   ],
-  exports: [HomepageModule, ProfileModule],
+  exports: [],
 })
 export class HomeWrapperModule {}
