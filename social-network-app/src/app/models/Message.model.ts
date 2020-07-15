8a45b1da20cc11b6ab02976c8476299aad5d96c8
Hamza Salvagno <hamza.salvagno@scai.intra>
2020-07-16 00:28:06 +0200

    create a model for message

---
+++ social-network-app/src/app/models/Message.model.ts
@@ -0,0 +1,12 @@
+import { Friend } from './Friend.model';
+import { User } from './User.model';
+
+export class Message extends Object {
+  id: number; // id of this message
+  created_time: string;
+  from: User; // userSecondVersionb in order to protect user privacy.
+  message: string;
+  subject: string;
+  tags: object[]; //  tags indicating the message
+  to: User[]; //  list of recipients of the message.
+}
