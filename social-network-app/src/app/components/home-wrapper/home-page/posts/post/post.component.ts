719274b98c98eb2efd510fcba7982f4c3cc78029
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-07-19 00:36:40 +0200

    change the graph of homepage posts (standardize)

--- social-network-app/src/app/components/home-wrapper/home-page/posts/post/post.component.ts
+++ social-network-app/src/app/components/home-wrapper/home-page/posts/post/post.component.ts
@@ -5,12 +5,22 @@ import { PostsService } from 'src/app/services/posts.service';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css'],
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent implements OnInit {
   @Input() post: Post;
-  loggedUserName = this.postService.loggedUserName;
-  constructor(private postService: PostsService) {}
+  loggedUserName = this.postsService.loggedUserName;
+  commentButtonClicked: boolean = false;
+  comments: string[];
+
+  constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {}
+
+  onCommentButtonClicked() {
+    this.commentButtonClicked = !this.commentButtonClicked;
+  }
+  onLikeButtonClicked(id: string) {
+    this.postsService.likePost(id);
+  }
 }
