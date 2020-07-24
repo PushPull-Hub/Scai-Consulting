38fa4bce06e5eb927bf08212fb837e521b335bca
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-07-24 09:14:28 +0200

    show user 5 friends suggestion on homepage

--- social-network-app/src/app/components/home-wrapper/home-page/friendships-suggestion/friendships-suggestion.component.ts
+++ social-network-app/src/app/components/home-wrapper/home-page/friendships-suggestion/friendships-suggestion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FriendsService } from 'src/app/services/friends.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserServices } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-friendships-suggestion',
@@ -6,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friendships-suggestion.component.scss'],
 })
 export class FriendshipsSuggestionComponent implements OnInit {
-  constructor() {}
+  friendsSuggestion: string[];
+  clicked = false;
+  constructor(
+    private friendsService: FriendsService,
+    private authService: AuthService,
+    private userService: UserServices
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.friendsSuggestion = this.friendsService
+      .getTenFriendsSuggestion()
+      .slice(0, 5);
+  }
+
+  getFriendProperty = (id: string, property: string) =>
+    this.userService.getaUserProperty(id, property);
+
+  addFriend(addedId) {
+    this.friendsService.addFriend(addedId);
+  }
 }
