f703e118e77a4ca92ae35a8e17342de946802d80
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-07-28 10:52:52 +0200

    style 1st component (messenger-navigation)

--- social-network-app/src/app/components/home-wrapper/messenger/messenger-wrapper/messenger-nav/messenger-nav.component.ts
+++ social-network-app/src/app/components/home-wrapper/messenger/messenger-wrapper/messenger-nav/messenger-nav.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-messenger-nav',
   templateUrl: './messenger-nav.component.html',
-  styleUrls: ['./messenger-nav.component.scss']
+  styleUrls: ['./messenger-nav.component.scss'],
 })
 export class MessengerNavComponent implements OnInit {
+  isMessageReaded: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
