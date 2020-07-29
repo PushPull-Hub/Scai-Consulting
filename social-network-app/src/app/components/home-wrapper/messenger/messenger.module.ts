57d95841e578e2a4b53be1e09819bd52beed08ed
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-07-29 12:28:22 +0200

    generate a pipe to filter the array message

--- social-network-app/src/app/components/home-wrapper/messenger/messenger.module.ts
+++ social-network-app/src/app/components/home-wrapper/messenger/messenger.module.ts
@@ -7,12 +7,14 @@ import { MessengerRoutingModule } from '../messenger/messenger-routing.module';
 import { MessengerNavComponent } from './messenger-wrapper/messenger-nav/messenger-nav.component';
 import { MessagesHandlerComponent } from './messenger-wrapper/messages-handler/messages-handler.component';
 import { MessengerWrapperComponent } from './messenger-wrapper/messenger-wrapper.component';
+import { MessagefilterPipe } from './messagefilter.pipe';
 
 @NgModule({
   declarations: [
     MessengerNavComponent,
     MessagesHandlerComponent,
     MessengerWrapperComponent,
+    MessagefilterPipe,
   ],
   imports: [CommonModule, SharedModule, MessengerRoutingModule],
 })
