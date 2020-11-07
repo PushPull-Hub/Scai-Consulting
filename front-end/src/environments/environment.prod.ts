a1a8b85e37fe996ef76b5208daf742536445716f
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-07 01:59:00 +0100

    set the root url as an envirnoment variable for global use

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  rootUrl: 'http://localhost:8080',
   male_avatar_photo_url: './assets/images/male-user.jpg',
   female_avatr_photo_url: './assets/images/female-user.jpg',
 };
--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  rootUrl: 'http://localhost:8080',
   male_avatar_photo_url: './assets/images/male-user.jpg',
   female_avatr_photo_url: './assets/images/female-user.jpg',
 };
