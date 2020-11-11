b7618b36e490f584bf1a613700721f08917345cf
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-11 20:46:09 +0100

    log author name in console as a signature

--- back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/ScaiChatApplication.java
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/ScaiChatApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScaiChatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScaiChatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("  _    _                                   \n" +
+                " | |  | |                                   \n" +
+                " | |__| |   __ _   _ __ ___    ____   __ _  \n" +
+                " |  __  |  / _` | | '_ ` _ \\  |_  /  / _`  |\n" +
+                " | |  | | | (_| | | | | | | |  / /  | (_| | \n" +
+                " |_|  |_|  \\__,_| |_| |_| |_| /___|  \\__,_ |");
+        SpringApplication.run(ScaiChatApplication.class, args);
+    }
 
 }
