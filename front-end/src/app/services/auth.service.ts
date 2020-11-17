557cd40351dac4e3c87e42d70eaf4822dd77f530
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-17 06:33:02 +0100

    mockup athentication service (wip)

--- front-end/src/app/services/auth.service.ts
+++ front-end/src/app/services/auth.service.ts
@@ -1,49 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+
 import { UserServices } from './user.service';
+
 import { User } from '../models/User.model';
 import { Profile } from '../models/Profile.model';
-import { Account } from '../models/Account.model';
+
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthService {
-  loggedUser: User;
+
+  private loggedUserSubject : BehaviorSubject<User> ;
+  public loggedUser : Observable<User>;
   isLoggedIn: boolean = false;
-  loggedProfile: Profile;
 
-  constructor(private router: Router, private userService: UserServices) {}
+  constructor( private http: HttpClient , private router: Router, private userService: UserServices) {
+    this.loggedUser = this.loggedUserSubject.asObservable();
+  }
 
-  logIn = (email: string, password: string): boolean => {
-    if (this.userService.usersList) {
-      const testedUser: User = this.userService.usersList.find(
-        (user) => user.email == email && user.password == password
-      );
-      if (testedUser) {
-        this.loggedUser = this.userService.getUserVersion2(testedUser.id);
-        this.userService.updateUser(this.loggedUser.id, 'isActive', true);
-        localStorage.setItem('loggedUserId', this.loggedUser.id);
-        return true;
-      }
-      return false;
+  logIn (email: string, password: string): boolean {
+
+    const profile = new Profile();
+    profile.email = email ;
+    profile.password = password ;
+
+    this.http.post<Profile>(
+        environment.rootUrl + `/api/profiles/profile`, profile 
+      ).subscribe( responseData => {
+        this.loggedUserSubject.next(responseData.user);
+        localStorage.setItem('loggedUserId', this.loggedUser.subscribe(user=> user.id).toString() );
+        // set user to active 
+      })
+      // return false;
+      return true;
     }
-
-    // const testUser = this.userService.getUser(email, password)
-    // .subscribe((resp) => {
-    //   this.loggedProfile = resp.profile;
-
-    //    this.loggedProfile.active = 1;
-    //    this.userService.updateProfile(this.loggedProfile);
-    //    localStorage.setItem('loggedUserId', this.loggedProfile.id);
-    //   console.log('true the logged profile  is : ' + this.loggedProfile);
-    //   return true;
-    // }
-    // console.log('false');
-    // return false;
   };
 
-  isAuthenticated() {
+  isAuthenticated ( ) : void  {
     this.isLoggedIn = localStorage.getItem('loggedUserId') ? true : false;
     return new Promise((resolve, reject) => {
       setTimeout(() => {
@@ -52,26 +51,14 @@ export class AuthService {
     });
   }
 
-  logOut = () => {
-    localStorage.removeItem('loggedUserId');
-    this.userService.updateUser(this.loggedUser.id, 'isActive', false);
-    this.loggedUser = null;
+  logOut () {
+    this.loggedUserSubject.next(null);
     this.isLoggedIn = false;
+    // update user to desactived 
+    localStorage.removeItem("loggedUserId")
     this.router.navigate(['/app/sign-in']);
-  };
-
-  getLoggedUserId() {
-    if (!this.loggedUser) {
-      const loggedId = localStorage.getItem('loggedUserId');
-      if (!loggedId) return null;
-      this.loggedUser = this.userService.getUserById(loggedId);
-    }
-    return this.loggedUser.id;
   }
 
-  getLoggedUser(): User {
-    if (!this.loggedUser) {
-      this.loggedUser = this.userService.getUserById(this.getLoggedUserId());
-    } else return this.loggedUser;
-  }
+
+
 }
