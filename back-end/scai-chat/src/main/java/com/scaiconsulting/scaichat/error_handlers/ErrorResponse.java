0f90ffba6102cc08ad8d42a600693ccb4e45351e
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-17 06:27:00 +0100

    create a custom error response

---
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/error_handlers/ErrorResponse.java
@@ -0,0 +1,41 @@
+package com.scaiconsulting.scaichat.error_handlers;
+
+public class ErrorResponse {
+
+    private int status ;
+    private String message ;
+    private Long timestamp ;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(int status, String message, Long timestamp) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
