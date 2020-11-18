7db20a9c5074daa9333fbd7560f0f82fd04aa2e4
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-18 23:51:54 +0100

    log the author name on console

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'social-network-app';
+  ngOnInit(): void {
+    console.log(
+      '  _    _                                   \n' +
+        ' | |  | |                                   \n' +
+        ' | |__| |   __ _   _ __ ___    ____   __ _  \n' +
+        " |  __  |  / _` | | '_ ` _ \\  |_  /  / _`  |\n" +
+        ' | |  | | | (_| | | | | | | |  / /  | (_| | \n' +
+        ' |_|  |_|  \\__,_| |_| |_| |_| /___|  \\__,_ |'
+    );
+  }
 }
