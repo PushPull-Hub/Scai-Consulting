bc6a73515b6f2eaf08b26242c369d25d8c2c974b
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-19 00:07:31 +0100

    auth (wip) - set a filter for auth request and execute it once

---
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/filters/AuthFilter.java
@@ -0,0 +1,39 @@
+package com.scaiconsulting.scaichat.filters;
+
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class AuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(httpServletRequest.getMethod())) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+
+        if ("/sign-in".equalsIgnoreCase(httpServletRequest.getRequestURI())) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+
+        String header = httpServletRequest.getHeader("Authentication");
+        if (header == null || !header.startsWith("Bearer")) {
+            throw new IllegalArgumentException("Header not found");
+
+        }
+        Jwts.parser().setSigningKey("scaiconsulting").parseClaimsJws(header.replace("Bearer ", "")).getBody();
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+    }
+}
