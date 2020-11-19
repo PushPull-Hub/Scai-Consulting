9e30d3337442fa241f1fd893efc361d2622e4c62
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-20 00:39:09 +0100

    auth (wip) - include name of header into expose-headers

--- back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/controllers/UserController.java
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/controllers/UserController.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = "Authentication")
 @RequestMapping("/api")
 public class UserController {
 
