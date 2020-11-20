46bfdde924685506d6f686457e2c2cf360f47d90
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-20 11:47:53 +0100

    handling errors while intercepting requests

--- front-end/src/app/interceptors/auth-interceptor.ts.interceptor.ts
+++ front-end/src/app/interceptors/auth-interceptor.ts.interceptor.ts
@@ -5,12 +5,17 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { catchError } from 'rxjs/operators';
+import { AlertService } from '../services/alert.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertService: AlertService
+  ) {}
   token: string = localStorage.getItem('token');
 
   intercept(
@@ -25,6 +30,16 @@ export class AuthInterceptor implements HttpInterceptor {
       });
     }
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if (err.status === 401) {
+          this.authService.logOut();
+          window.location.reload();
+        }
+        const error = err.error.message || err.statusText;
+        this.alertService.error(error);
+        return throwError(error);
+      })
+    );
   }
 }
