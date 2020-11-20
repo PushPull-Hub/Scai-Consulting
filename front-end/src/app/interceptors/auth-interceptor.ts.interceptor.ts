c94b5d05cf1367b111d96f871a6fe4216ef78d9a
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-20 03:47:37 +0100

    intercept requests and add token to each

--- front-end/src/app/interceptors/auth-interceptor.ts.interceptor.ts
+++ front-end/src/app/interceptors/auth-interceptor.ts.interceptor.ts
@@ -6,16 +6,25 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private authService: AuthService) {}
+  token: string = localStorage.getItem('token');
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    console.log("Request is on it's way ");
+    if (this.token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      });
+    }
+
     return next.handle(request);
   }
 }
