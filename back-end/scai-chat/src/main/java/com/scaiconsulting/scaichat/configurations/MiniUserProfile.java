a298e913577cae434e698ce609472ce2a71f3a1a
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-11-30 17:47:19 +0100

    create a DTO

---
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/configurations/MiniUserProfile.java
@@ -0,0 +1,62 @@
+package com.scaiconsulting.scaichat.configurations;
+
+public class MiniUserProfile {
+
+    private int Id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private boolean active;
+
+
+    public MiniUserProfile() {
+    }
+
+    public MiniUserProfile(int id, String firstName, String lastName, String gender, boolean active) {
+        Id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.active = active;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
