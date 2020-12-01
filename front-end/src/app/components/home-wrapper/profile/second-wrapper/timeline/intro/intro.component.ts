bf17489906c8e39fed8b43a9bf10493aa8a3dd54
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-01 15:13:52 +0100

    comment code snippets to avoid compiler issues

--- front-end/src/app/components/home-wrapper/profile/second-wrapper/timeline/intro/intro.component.ts
+++ front-end/src/app/components/home-wrapper/profile/second-wrapper/timeline/intro/intro.component.ts
@@ -10,34 +10,34 @@ import { User } from 'src/app/models/User.model';
   styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent implements OnInit {
-  EditButtonClicked: boolean = false;
+  // EditButtonClicked: boolean = false;
 
-  location: string =
-    this.userService.getaUserProperty(
-      this.authService.getLoggedUserId(),
-      'location'
-    ) || '';
-  hometown: string =
-    this.userService.getaUserProperty(
-      this.authService.getLoggedUserId(),
-      'hometown'
-    ) || '';
-  work_in: string =
-    this.userService.getaUserProperty(
-      this.authService.getLoggedUserId(),
-      'work_in'
-    ) || '';
-  birthdayObject: {
-    year: string;
-    month: string;
-    day: string;
-  } = this.userService.getaUserProperty(
-    this.authService.getLoggedUserId(),
-    'birthday'
-  );
-  birthday: string = this.birthdayObject.year
-    ? `${this.birthdayObject.year}/${this.birthdayObject.month}/${this.birthdayObject.day}`
-    : '';
+  // location: string =
+  //   this.userService.getaUserProperty(
+  //     this.authService.getLoggedUserId(),
+  //     'location'
+  //   ) || '';
+  // hometown: string =
+  //   this.userService.getaUserProperty(
+  //     this.authService.getLoggedUserId(),
+  //     'hometown'
+  //   ) || '';
+  // work_in: string =
+  //   this.userService.getaUserProperty(
+  //     this.authService.getLoggedUserId(),
+  //     'work_in'
+  //   ) || '';
+  // birthdayObject: {
+  //   year: string;
+  //   month: string;
+  //   day: string;
+  // } = this.userService.getaUserProperty(
+  //   this.authService.getLoggedUserId(),
+  //   'birthday'
+  // );
+  // birthday: string = this.birthdayObject.year
+  //   ? `${this.birthdayObject.year}/${this.birthdayObject.month}/${this.birthdayObject.day}`
+  //   : '';
 
   constructor(
     private userService: UserServices,
@@ -47,40 +47,40 @@ export class IntroComponent implements OnInit {
   ngOnInit(): void {}
 
   showInputs = () => {
-    this.EditButtonClicked = true;
+    // this.EditButtonClicked = true;
   };
-  editUserInfos = (f: NgForm) => {
-    const location = f.value.location;
-    const hometown = f.value.hometown;
-    const work_in = f.value.work_in;
-    const birthday = f.value.birthday;
+  // editUserInfos = (f: NgForm) => {
+  //   const location = f.value.location;
+  //   const hometown = f.value.hometown;
+  //   const work_in = f.value.work_in;
+  //   const birthday = f.value.birthday;
 
-    this.location = location;
-    this.hometown = hometown;
-    this.work_in = work_in;
-    this.birthday = birthday;
+  //   this.location = location;
+  //   this.hometown = hometown;
+  //   this.work_in = work_in;
+  //   this.birthday = birthday;
 
-    this.userService.updateUser(
-      this.authService.getLoggedUserId(),
-      'location',
-      location
-    );
-    this.userService.updateUser(
-      this.authService.getLoggedUserId(),
-      'hometown',
-      hometown
-    );
-    this.userService.updateUser(
-      this.authService.getLoggedUserId(),
-      'work_in',
-      work_in
-    );
-    this.userService.updateUser(
-      this.authService.getLoggedUserId(),
-      'birthday',
-      birthday
-    );
+  //   this.userService.updateUser(
+  //     this.authService.getLoggedUserId(),
+  //     'location',
+  //     location
+  //   );
+  //   this.userService.updateUser(
+  //     this.authService.getLoggedUserId(),
+  //     'hometown',
+  //     hometown
+  //   );
+  //   this.userService.updateUser(
+  //     this.authService.getLoggedUserId(),
+  //     'work_in',
+  //     work_in
+  //   );
+  //   this.userService.updateUser(
+  //     this.authService.getLoggedUserId(),
+  //     'birthday',
+  //     birthday
+  //   );
 
-    this.EditButtonClicked = false;
-  };
+  //   this.EditButtonClicked = false;
+  // };
 }
