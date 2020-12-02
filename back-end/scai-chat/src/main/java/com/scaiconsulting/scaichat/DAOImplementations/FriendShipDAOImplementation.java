4b9bb3a136c5b65fd878d32abf2c7f920ec5d2cf
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-02 23:58:30 +0100

    improve the data access object implementations methods

--- back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/DAOImplementations/FriendShipDAOImplementation.java
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/DAOImplementations/FriendShipDAOImplementation.java
@@ -54,120 +54,29 @@ public class FriendShipDAOImplementation implements FriendShipDAO {
         return friendShip;
     }
 
+    @Override
+    public FriendShip acceptFriendRequest(FriendShip friendShip) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(friendShip);
+        return friendShip;
+    }
 
-//    @Override
-//    public List<FriendShip> getFriendShipList(int userId) {
-//        Session currentSession = entityManager.unwrap(Session.class);
-//        Query<FriendShip> theQuery = currentSession.createQuery("from FriendShip f where f.firstUserId =:userId or secondUserId =:userId", FriendShip.class)
-//                .setParameter("userId", userId);
-//        return theQuery.getResultList();
-//    }
-//
-//  /*  @Override
-//    public FriendShip getFriendShipByItsId(int id) {
-//        Session currentSession = entityManager.unwrap(Session.class);
-//        Query<FriendShip> theQuery = currentSession.createQuery("from FriendShip f where f.id =:id", FriendShip.class)
-//                .setParameter("id", id);
-//        return theQuery.getSingleResult();
-//    }
-//
-//    @Override
-//    public FriendShip getFriendShipByFriendId(int friendId) {
-//        Session currentSession = entityManager.unwrap(Session.class);
-//        Query<FriendShip> theQuery = currentSession.createQuery("from FriendShip f where f.firstUserId =:friendId or secondUserId =:friendId", FriendShip.class)
-//                .setParameter("friendId", friendId);
-//        return theQuery.getSingleResult();
-//
-//    }
-//
-//    @Override
-//    public FriendShip createFriendShip(FriendShip friendShip) {
-//        Session currentSession = entityManager.unwrap(Session.class);
-//        currentSession.saveOrUpdate(friendShip);
-//        return friendShip;
-//    }*/
-//
-//    @Override
-//    public List<User> getTenFriendsSuggestion(int userId) {
-//        Session currentSession = entityManager.unwrap(Session.class);
-//        List<FriendShip> userFriendShips = this.getFriendShipList(userId);
-//        ArrayList<Integer> friendsId = new ArrayList<>();
-//        for (FriendShip friendShip : userFriendShips) {
-//            if (friendShip.getFirstUserId() == userId) {
-//                friendsId.add(friendShip.getSecondUserId());
-//            } else {
-//                friendsId.add(friendShip.getFirstUserId());
-//            }
-//        }
-//        Query<User> theNotFriendsQuery = currentSession.createQuery("select  from User u where u.id NOT IN (:friendsId) ", User.class);
-//        theNotFriendsQuery.setParameter("friendsId", friendsId).setMaxResults(10);
-//        return theNotFriendsQuery.getResultList();
-//    }
-//
-//    @Override
-//    public FriendShip sendFriendRequest(FriendShip friendShip) {
-//        Session currentSession = entityManager.unwrap(Session.class);
-//        currentSession.saveOrUpdate(friendShip);
-//        return friendShip;
-//    }
-//
-//    @Override
-//    public int acceptFriendRequest(int acceptorId, int senderId) {
-//        int updateResult = 0;
-//        Session currentSession = entityManager.unwrap(Session.class);
-//
-//        if (acceptorId < senderId) {
-//            Query tehQuery = currentSession.createQuery("update FriendShip f set f.status=:Accepted , f.actionUserId =:acceptorId where f.firstUserId =:acceptorId and f.secondUserId=:senderId ");
-//            tehQuery.setParameter("Accepted", 1);
-//            tehQuery.setParameter("acceptorId", acceptorId);
-//            tehQuery.setParameter("senderId", senderId);
-//            updateResult = tehQuery.executeUpdate();
-//        } else if (acceptorId > senderId) {
-//            Query tehQuery = currentSession.createQuery("update FriendShip f set f.status=:Accepted , f.actionUserId =:acceptorId where f.firstUserId =:senderId and f.secondUserId=:acceptorId");
-//            tehQuery.setParameter("Accepted", 1);
-//            tehQuery.setParameter("acceptorId", acceptorId);
-//            tehQuery.setParameter("senderId", senderId);
-//            updateResult = tehQuery.executeUpdate();
-//        }
-//
-//        return updateResult;
-//    }
-//
-//    @Override
-//    public FriendShip checkFriendShip(int user_id_one, int user_id_two) {
-//        Session currentSession = entityManager.unwrap(Session.class);
-//        Query<FriendShip> theQuery = currentSession.createQuery("from FriendShip f where f.firstUserId =:user_id_one and  f.secondUserId=:user_id_two ",FriendShip.class);
-//        theQuery.setParameter("user_id_one",user_id_one);
-//        theQuery.setParameter("user_id_two",user_id_two);
-//        return theQuery.getSingleResult();
-//    }
-//
-//
-//    @Override
-//    public List<FriendShip> getPendingFriendRequests(int userId) {
-//        Session currentSession = entityManager.unwrap(Session.class);
-//        Query<FriendShip> theQuery = currentSession.createQuery("from FriendShip f where f.actionUserId=:userId and f.status=:Pending " , FriendShip.class);
-//        theQuery.setParameter("userId",userId);
-//        theQuery.setParameter("Pending",0);
-//        return theQuery.getResultList() ;
-//    }
-//
-//    @Override
-//    public int blockFriend(int userId, int friendId) {
-//        Session currentSession = entityManager.unwrap(Session.class);
-//
-//        if (userId < friendId){
-//            Query<FriendShip> theQuery = currentSession.createQuery("update FriendShip f set f.status=:Blocked, f.actionUserId=:userId where  f.firstUserId =:userId and f.secondUserId=:friendId ",FriendShip.class);
-//            theQuery.setParameter("Blocked",3);
-//            theQuery.setParameter("userId",userId);
-//            theQuery.setParameter("friendId",friendId);
-//        }else if (userId > friendId){
-//
-//        }
-//
-//
-//        return 0;
-//    }
+    @Override
+    public List<FriendShip> getPendingFriendRequests(int userId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<FriendShip> theQuery = currentSession.createQuery("from FriendShip where firstUserId=:userId and status =:Pending  and actionUserId=:userId" +
+                " or secondUserId=:userId and status =:Pending  and actionUserId=:userId ", FriendShip.class);
+        theQuery.setParameter("userId", userId);
+        theQuery.setParameter("Pending", 0);
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public FriendShip updateFriendShip(FriendShip friendShip) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.update(friendShip);
+        return friendShip;
+    }
 
 
 // Status Codes
