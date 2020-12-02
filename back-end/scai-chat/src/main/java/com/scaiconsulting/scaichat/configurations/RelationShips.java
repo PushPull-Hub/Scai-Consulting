9a4b37e6df8f08cdfba105596a6d5f2a3e209f8f
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-02 23:56:03 +0100

    create a data transfer object to wrapp all the relations user may have

---
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/configurations/RelationShips.java
@@ -0,0 +1,69 @@
+package com.scaiconsulting.scaichat.configurations;
+
+import com.scaiconsulting.scaichat.entities.FriendShip;
+
+import java.util.ArrayList;
+
+public class RelationShips {
+
+    ArrayList<FriendShip> myFriends;
+
+    ArrayList<FriendShip> PendingRequests;
+
+    ArrayList<FriendShip> blockedMe;
+
+    ArrayList<FriendShip> DeclinedRequests;
+
+    ArrayList<FriendShip> blockedBy;
+
+    public RelationShips() {
+    }
+
+    public RelationShips(ArrayList<FriendShip> myFriends, ArrayList<FriendShip> pendingRequests, ArrayList<FriendShip> blockedMe, ArrayList<FriendShip> declinedRequests, ArrayList<FriendShip> blockedBy) {
+        this.myFriends = myFriends;
+        PendingRequests = pendingRequests;
+        this.blockedMe = blockedMe;
+        DeclinedRequests = declinedRequests;
+        this.blockedBy = blockedBy;
+    }
+
+    public ArrayList<FriendShip> getMyFriends() {
+        return myFriends;
+    }
+
+    public void setMyFriends(ArrayList<FriendShip> myFriends) {
+        this.myFriends = myFriends;
+    }
+
+    public ArrayList<FriendShip> getPendingRequests() {
+        return PendingRequests;
+    }
+
+    public void setPendingRequests(ArrayList<FriendShip> pendingRequests) {
+        PendingRequests = pendingRequests;
+    }
+
+    public ArrayList<FriendShip> getBlockedMe() {
+        return blockedMe;
+    }
+
+    public void setBlockedMe(ArrayList<FriendShip> blockedMe) {
+        this.blockedMe = blockedMe;
+    }
+
+    public ArrayList<FriendShip> getDeclinedRequests() {
+        return DeclinedRequests;
+    }
+
+    public void setDeclinedRequests(ArrayList<FriendShip> declinedRequests) {
+        DeclinedRequests = declinedRequests;
+    }
+
+    public ArrayList<FriendShip> getBlockedBy() {
+        return blockedBy;
+    }
+
+    public void setBlockedBy(ArrayList<FriendShip> blockedBy) {
+        this.blockedBy = blockedBy;
+    }
+}
