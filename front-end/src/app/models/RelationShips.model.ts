35c50c3471c24239034eeea0206c1649d9dbc5b6
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-04 10:03:21 +0100

    generate a model to wrap all users relationships

---
+++ front-end/src/app/models/RelationShips.model.ts
@@ -0,0 +1,9 @@
+import { FriendShip } from './FriendShip.model';
+
+export class RelationShips extends Object {
+  myFriends: FriendShip[];
+  pendingRequests: FriendShip[];
+  blockedMe: FriendShip[];
+  declinedRequests: FriendShip[];
+  blockedBy: FriendShip[];
+}
