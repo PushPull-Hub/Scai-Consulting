35d64f4ccf6c90d292ee81a6a262ef90cd9986c2
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-08 20:05:45 +0100

    manage async methods after recieving data from parent component for other child

--- front-end/src/app/components/home-wrapper/friends/blocked/blocked.component.ts
+++ front-end/src/app/components/home-wrapper/friends/blocked/blocked.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { MiniProfile } from 'src/app/models/MiniProfile.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-blocked',
   templateUrl: './blocked.component.html',
-  styleUrls: ['./blocked.component.scss']
+  styleUrls: ['./blocked.component.scss'],
 })
 export class BlockedComponent implements OnInit {
+  male_avatar_photo_url: string;
+  loading: boolean = true;
+  BlockedByMeListProfiles: MiniProfile[];
+  doIhaveBlockedUsersByMe: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.male_avatar_photo_url = environment.male_avatar_photo_url;
   }
 
+  loadProfiles(data) {
+    setTimeout(() => {
+      if (data) {
+        this.BlockedByMeListProfiles = data;
+        this.doIhaveBlockedUsersByMe = true;
+        this.loading = false;
+      } else {
+        this.loading = false;
+        this.doIhaveBlockedUsersByMe = false;
+      }
+    }, 400);
+  }
 }
--- front-end/src/app/components/home-wrapper/friends/pending-requests/pending-requests.component.ts
+++ front-end/src/app/components/home-wrapper/friends/pending-requests/pending-requests.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { MiniProfile } from 'src/app/models/MiniProfile.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-pending-requests',
   templateUrl: './pending-requests.component.html',
-  styleUrls: ['./pending-requests.component.scss']
+  styleUrls: ['./pending-requests.component.scss'],
 })
 export class PendingRequestsComponent implements OnInit {
+  pendingRequestsProfiles: MiniProfile[];
+  doIhavePendingFriendRequests: boolean = true;
+  loading: boolean = true;
+  male_avatar_photo_url: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.male_avatar_photo_url = environment.male_avatar_photo_url;
   }
 
+  loadProfiles(data) {
+    setTimeout(() => {
+      if (data) {
+        this.pendingRequestsProfiles = data;
+        this.doIhavePendingFriendRequests = true;
+        this.loading = false;
+      } else {
+        this.doIhavePendingFriendRequests = false;
+        this.loading = false;
+      }
+    }, 400);
+  }
 }
