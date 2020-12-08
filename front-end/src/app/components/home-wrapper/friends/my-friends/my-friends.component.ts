bd653c3fd41ba2cb4515a551a72180a33fcb3544
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-08 20:03:06 +0100

    manage async methods after recieving data from parent component

--- front-end/src/app/components/home-wrapper/friends/my-friends/my-friends.component.ts
+++ front-end/src/app/components/home-wrapper/friends/my-friends/my-friends.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MiniProfile } from 'src/app/models/MiniProfile.model';
 import { environment } from 'src/environments/environment';
 
@@ -8,11 +8,33 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./my-friends.component.scss'],
 })
 export class MyFriendsComponent implements OnInit {
-  @Input() friendProfile: MiniProfile;
   male_avatar_photo_url: string;
+  myFriendsProfiles: MiniProfile[];
+  loading: boolean = true;
+  doIhaveFriends: boolean = true;
+  messageIconClicked: boolean;
+
   constructor() {}
 
   ngOnInit(): void {
     this.male_avatar_photo_url = environment.male_avatar_photo_url;
+    this.messageIconClicked = false;
+  }
+
+  loadProfiles(data) {
+    setTimeout(() => {
+      if (data) {
+        this.myFriendsProfiles = data;
+        this.doIhaveFriends = true;
+        this.loading = false;
+      } else {
+        this.loading = false;
+        this.doIhaveFriends = false;
+      }
+    }, 400);
+  }
+
+  getConversation() {
+    this.messageIconClicked = !this.messageIconClicked;
   }
 }
