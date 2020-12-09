a8a3d25b6a2a753a9f49342d88a211d50c3e2c93
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-09 16:14:47 +0100

    create template and add accept friend method

--- front-end/src/app/components/home-wrapper/friends/requests/requests.component.ts
+++ front-end/src/app/components/home-wrapper/friends/requests/requests.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FriendShip } from 'src/app/models/FriendShip.model';
+import { MiniProfile } from 'src/app/models/MiniProfile.model';
+import { FriendsService } from 'src/app/services/friends.service';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-requests',
   templateUrl: './requests.component.html',
-  styleUrls: ['./requests.component.scss']
+  styleUrls: ['./requests.component.scss'],
 })
 export class RequestsComponent implements OnInit {
+  male_avatar_photo_url: string;
+  loading: boolean = true;
+  requesterProfiles: MiniProfile[];
+  doIhaveRequests: boolean = true;
 
-  constructor() { }
+  @Output() friendShipEmitter: EventEmitter<any> = new EventEmitter();
+
+  constructor(private friendService: FriendsService) {}
 
   ngOnInit(): void {
+    this.male_avatar_photo_url = environment.male_avatar_photo_url;
   }
 
+  loadProfiles(data) {
+    setTimeout(() => {
+      if (data) {
+        this.requesterProfiles = data;
+        this.doIhaveRequests = true;
+        this.loading = false;
+      } else {
+        this.loading = false;
+        this.doIhaveRequests = false;
+      }
+    }, 400);
+  }
+
+  acceptFriendRequest(requestorId: number) {
+    this.friendService
+      .acceptFriendRequest(requestorId)
+      .subscribe((result: FriendShip) => {
+        if (result) {
+          this.friendShipEmitter.emit(result);
+          setTimeout(() => {
+            const p = this.requesterProfiles.filter(
+              (profile: MiniProfile) => profile.id !== requestorId
+            );
+            this.requesterProfiles = p;
+          }, 500);
+        } else console.log('check conditions ');
+      });
+  }
+
+  declineFriendRequest() {}
 }
