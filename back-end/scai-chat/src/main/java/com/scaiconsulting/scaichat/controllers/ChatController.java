11d20eedf82bd42a5536e5287b538ad80fdcc3a0
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-11 10:14:03 +0100

    resest the chat controller

--- back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/controllers/ChatController.java
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/controllers/ChatController.java
@@ -1,12 +1,16 @@
 package com.scaiconsulting.scaichat.controllers;
 
+import com.scaiconsulting.scaichat.configurations.Chat;
+import com.scaiconsulting.scaichat.configurations.MessageDTO;
 import com.scaiconsulting.scaichat.entities.Conversation;
+import com.scaiconsulting.scaichat.entities.Message;
 import com.scaiconsulting.scaichat.exeptions.NotFoundException;
 import com.scaiconsulting.scaichat.services.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = "Authentication")
@@ -20,30 +24,43 @@ public class ChatController {
         this.chatService = chatService;
     }
 
-    @PostMapping(name = "/conversation")
+    @PostMapping("/chats")
     public Conversation createConversation(@RequestHeader("Authentication") String token, @RequestBody int friendId) {
-        return this.chatService.createConversation(token, friendId);
+        return chatService.createConversation(token,friendId);
     }
 
-    @GetMapping(name = "/conversation")
-    public Conversation getConversation(@RequestHeader("Authentication") String token, @RequestBody int friendId) {
-        return this.chatService.createConversation(token, friendId);
+
+
+    @PostMapping("/conversation")
+    public Chat getConversationByUsersIds(@RequestHeader("Authentication") String token, @RequestBody int friendId) {
+        if (this.chatService.getConversationByUsersIds(token, friendId) != null) {
+
+            return this.chatService.getConversationByUsersIds(token, friendId);
+        } else throw new NotFoundException("can't find user with id :" + friendId);
     }
 
-    @GetMapping(name = "/conversations")
-    public List<Conversation> getConversations(String token) {
+    @GetMapping("/conversation/{id}")
+    public Chat getConversationByItsId(@RequestHeader("Authentication") String token,@PathVariable int id) {
+        if (this.chatService.getConversationByItsId(token,id) != null) {
+            return this.chatService.getConversationByItsId(token,id);
+        } else throw new NotFoundException("can't find conversation with id :" + id);
+    }
+
+    @GetMapping("/chats")
+    public List<Conversation> getConversations(@RequestHeader("Authentication") String token) {
         return this.chatService.getConversations(token);
     }
 
-    @PutMapping(name = "/conversation")
-    public Conversation updateConversation(@RequestHeader("Authentication") String token, @RequestBody Conversation conversation) {
-        Conversation result = this.chatService.getConversation(conversation.getId());
-        if (conversation.getId() > 0) {
-            if (result != null) {
-                return this.chatService.updateConversation(conversation);
-            } else throw new NotFoundException("can't find conversation with id :" + conversation.getId());
-        } else throw new NotFoundException("conversation needs to include an id");
+    @PostMapping("/messages/message")
+    public Message sendMessage(@RequestHeader("Authentication") String token, @RequestBody MessageDTO message) {
+       return  this.chatService.sendMessage(token, message);
     }
+
+    @PostMapping("/see-last-message")
+    public Conversation setConversationLastMessageToSeen(@RequestHeader("Authentication") String token ,@RequestBody int conversationId){
+        return this.chatService.setConversationLastMessageToSeen(conversationId);
+    }
+
 
 
 }
