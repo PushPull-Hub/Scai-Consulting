50bd2a8087ed81754d1b436dbfae9908263fbe47
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-11 10:16:08 +0100

    add and improve methods in the service

--- back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/services/ChatService.java
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/services/ChatService.java
@@ -1,6 +1,9 @@
 package com.scaiconsulting.scaichat.services;
 
+import com.scaiconsulting.scaichat.configurations.Chat;
+import com.scaiconsulting.scaichat.configurations.MessageDTO;
 import com.scaiconsulting.scaichat.entities.Conversation;
+import com.scaiconsulting.scaichat.entities.Message;
 
 import java.util.List;
 
@@ -8,13 +11,25 @@ public interface ChatService {
 
     Conversation createConversation(String token, int secondUserId);
 
-    Conversation getConversation(String token, int secondUserId);
+    Chat getConversationByUsersIds(String token, int secondUserId);
 
-    Conversation getConversation(int conversationId);
+    Chat getConversationByItsId(String token , int conversationId);
 
     List<Conversation> getConversations(String token);
 
     Conversation updateConversation(Conversation conversation );
 
     int deleteConversation(int conversationId);
+
+    Message getLastMessage(int conversationId);
+
+    Message getMessage(int messageId);
+
+    Message sendMessage(String token , MessageDTO message);
+
+    Conversation setConversationLastMessageToSeen (int conversationId);
+
+    Message updateMessage(Message message);
+
+    int deleteMessage (int messageId);
 }
--- back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/servicesImplemetations/ChatServiceImplementation.java
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/servicesImplemetations/ChatServiceImplementation.java
@@ -2,18 +2,21 @@ package com.scaiconsulting.scaichat.servicesImplemetations;
 
 import com.scaiconsulting.scaichat.DAOs.ChatDao;
 import com.scaiconsulting.scaichat.DAOs.UserDAO;
+import com.scaiconsulting.scaichat.configurations.Chat;
 import com.scaiconsulting.scaichat.configurations.IdExtractor;
+import com.scaiconsulting.scaichat.configurations.MessageDTO;
+import com.scaiconsulting.scaichat.configurations.MiniUserProfile;
 import com.scaiconsulting.scaichat.entities.Conversation;
-import com.scaiconsulting.scaichat.exeptions.NotFoundException;
+import com.scaiconsulting.scaichat.entities.Message;
 import com.scaiconsulting.scaichat.services.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
-@Transactional
 public class ChatServiceImplementation implements ChatService {
 
     private final ChatDao chatDao;
@@ -26,42 +29,158 @@ public class ChatServiceImplementation implements ChatService {
     }
 
     @Override
+    @Transactional
     public Conversation createConversation(String token, int secondUserId) {
         if (userDAO.getUser(secondUserId) != null) {
             int userId = new IdExtractor(token).getAuthenticatedUserId();
             Conversation conversation = new Conversation();
             conversation.setFirstUserId(userId);
-            conversation.setSecondUserId(userId);
+            conversation.setSecondUserId(secondUserId);
             return this.chatDao.createConversation(conversation);
-        } else throw new NotFoundException("can't find user with id :" + secondUserId);
+        } else return null;
     }
 
     @Override
-    public Conversation getConversation(String token, int secondUserId) {
+    @Transactional
+    public Chat getConversationByUsersIds(String token, int secondUserId) {
         int userId = new IdExtractor(token).getAuthenticatedUserId();
         if (userDAO.getUser(secondUserId) != null) {
-            return this.chatDao.getConversation(userId, secondUserId);
-        } else throw new NotFoundException("can't find user with id :" + secondUserId);
+            Chat chat = new Chat();
+            Conversation conversation = this.chatDao.getConversationByUsersIds(userId, secondUserId);
+            MiniUserProfile myFriendProfile;
+
+            Message lastMessage = this.getLastMessage(conversation.getId());
+            myFriendProfile = this.userDAO.getMiniUserProfile(secondUserId);
+            if (conversation.getFirstUserId() == userId) {
+                chat.setId(conversation.getId());
+                chat.setMyId(conversation.getFirstUserId());
+                chat.setSecondUser(myFriendProfile);
+                chat.setIsLastMessageSeen(conversation.getIsLastMessageSeen());
+                if (this.getLastMessage(conversation.getId()) != null) {
+                    chat.setLastMessage(lastMessage);
+                } else {
+                    chat.setLastMessage(null);
+                }
+
+            } else if (conversation.getFirstUserId() != userId) {
+                myFriendProfile = this.userDAO.getMiniUserProfile(userId);
+                chat.setId(conversation.getId());
+                chat.setMyId(conversation.getSecondUserId());
+                chat.setSecondUser(myFriendProfile);
+                chat.setIsLastMessageSeen(conversation.getIsLastMessageSeen());
+                if (this.getLastMessage(conversation.getId()) != null) {
+                    chat.setLastMessage(lastMessage);
+                } else {
+                    chat.setLastMessage(null);
+                }
+            }
+            return chat;
+        } else return null;
     }
 
     @Override
-    public Conversation getConversation(int conversationId) {
-        return this.chatDao.getConversation(conversationId);
+    @Transactional
+    public Chat getConversationByItsId(String token, int conversationId) {
+        int userId = new IdExtractor(token).getAuthenticatedUserId();
+        Chat chat = new Chat();
+        if (this.chatDao.getConversationByItsId(conversationId) != null) {
+            Conversation conversation = this.chatDao.getConversationByItsId(conversationId);
+            MiniUserProfile myFriendProfile;
+
+            Message lastMessage = this.getLastMessage(conversation.getId());
+
+            if (conversation.getFirstUserId() == userId) {
+                chat.setId(conversation.getId());
+                chat.setMyId(conversation.getFirstUserId());
+                chat.setSecondUser(this.userDAO.getMiniUserProfile(conversation.getSecondUserId()));
+                chat.setIsLastMessageSeen(conversation.getIsLastMessageSeen());
+                if (this.getLastMessage(conversation.getId()) != null) {
+                    chat.setLastMessage(lastMessage);
+                } else {
+                    chat.setLastMessage(null);
+                }
+
+            } else if (conversation.getFirstUserId() != userId) {
+                myFriendProfile = this.userDAO.getMiniUserProfile(conversation.getFirstUserId());
+                chat.setId(conversation.getId());
+                chat.setMyId(conversation.getSecondUserId());
+                chat.setSecondUser(myFriendProfile);
+                chat.setIsLastMessageSeen(conversation.getIsLastMessageSeen());
+                if (this.getLastMessage(conversation.getId()) != null) {
+                    chat.setLastMessage(lastMessage);
+                } else {
+                    chat.setLastMessage(null);
+                }
+            }
+            return chat;
+
+        } else return null;
     }
 
     @Override
+    @Transactional
     public List<Conversation> getConversations(String token) {
         int userId = new IdExtractor(token).getAuthenticatedUserId();
         return this.chatDao.getConversations(userId);
     }
 
     @Override
+    @Transactional
     public Conversation updateConversation(Conversation conversation) {
         return this.chatDao.updateConversation(conversation);
     }
 
     @Override
+    @Transactional
     public int deleteConversation(int conversationId) {
         return this.chatDao.deleteConversation(conversationId);
+    }
+
+    @Override
+    @Transactional
+    public Message getLastMessage(int conversationId) {
+        return this.chatDao.getLastMessage(conversationId);
+    }
+
+    @Override
+    @Transactional
+    public Message sendMessage(String token, MessageDTO message) {
+        Conversation conversation = this.chatDao.getConversationByItsId(message.getConversionId());
+        Message theMessage = new Message();
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        int userId = new IdExtractor(token).getAuthenticatedUserId();
+        theMessage.setId(0);
+        theMessage.setSenderId(userId);
+        theMessage.setCreatedTime(now);
+        theMessage.setConversation(conversation);
+        theMessage.setReceiverId(message.getReceiverId());
+        theMessage.setText(message.getText());
+        return this.chatDao.createMessage(theMessage);
+    }
+
+    @Override
+    @Transactional
+    public Conversation setConversationLastMessageToSeen(int conversationId) {
+        Conversation theConversation = this.chatDao.getConversationByItsId(conversationId);
+        theConversation.setIsLastMessageSeen(1);
+        return this.chatDao.updateConversation(theConversation);
+    }
+
+    @Override
+    @Transactional
+    public Message getMessage(int messageId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Message updateMessage(Message message) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public int deleteMessage(int messageId) {
+        return 0;
     }
 }
