15b43607862aabec25cd2ae603ef752db27c2468
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-11 10:14:49 +0100

    create 2 DTOs to send needed data to front-end

---
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/configurations/Chat.java
@@ -0,0 +1,84 @@
+package com.scaiconsulting.scaichat.configurations;
+
+import com.scaiconsulting.scaichat.entities.Message;
+
+import java.util.ArrayList;
+
+public class Chat {
+    private int id;
+    private int myId;
+    private MiniUserProfile secondUser;
+    private int isLastMessageSeen;
+    private Message lastMessage;
+    ArrayList<Message> messages;
+
+
+    public Chat() {
+    }
+
+    public Chat(int id, int myId, MiniUserProfile secondUser, int isLastMessageSeen, Message lastMessage) {
+        this.id = id;
+        this.myId = myId;
+        this.secondUser = secondUser;
+        this.isLastMessageSeen = isLastMessageSeen;
+        this.lastMessage = lastMessage;
+    }
+
+    public Chat(int id, int myId, MiniUserProfile secondUser, int isLastMessageSeen, Message lastMessage, ArrayList<Message> messages) {
+        this.id = id;
+        this.myId = myId;
+        this.secondUser = secondUser;
+        this.isLastMessageSeen = isLastMessageSeen;
+        this.lastMessage = lastMessage;
+        this.messages = messages;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMyId() {
+        return myId;
+    }
+
+    public void setMyId(int myId) {
+        this.myId = myId;
+    }
+
+    public MiniUserProfile getSecondUser() {
+        return secondUser;
+    }
+
+    public void setSecondUser(MiniUserProfile secondUser) {
+        this.secondUser = secondUser;
+    }
+
+    public int getIsLastMessageSeen() {
+        return isLastMessageSeen;
+    }
+
+    public void setIsLastMessageSeen(int isLastMessageSeen) {
+        this.isLastMessageSeen = isLastMessageSeen;
+    }
+
+    public Message getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(Message lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+
+    public ArrayList<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(ArrayList<Message> messages) {
+        this.messages = messages;
+    }
+}
---
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/configurations/MessageDTO.java
@@ -0,0 +1,56 @@
+package com.scaiconsulting.scaichat.configurations;
+
+public class MessageDTO {
+
+    private int senderId;
+
+    private int receiverId;
+
+    private String text;
+
+    private int conversionId;
+
+
+    public MessageDTO() {
+    }
+
+    public MessageDTO(int senderId, int receiverId, String text, int conversionId) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.text = text;
+        this.conversionId = conversionId;
+    }
+
+
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    public int getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(int receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getConversionId() {
+        return conversionId;
+    }
+
+    public void setConversionId(int conversionId) {
+        this.conversionId = conversionId;
+    }
+}
