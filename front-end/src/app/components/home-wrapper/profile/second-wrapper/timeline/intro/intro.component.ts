51924af8e273550033a2bfe213c7c2a2c7432ad2
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-11 10:18:37 +0100

    reset the intro component and add the requird method to update

--- front-end/src/app/components/home-wrapper/profile/second-wrapper/timeline/intro/intro.component.ts
+++ front-end/src/app/components/home-wrapper/profile/second-wrapper/timeline/intro/intro.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserServices } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/services/auth.service';
-import { User } from 'src/app/models/User.model';
+import { Birthday, User } from 'src/app/models/User.model';
 
 @Component({
   selector: 'app-intro',
@@ -10,77 +10,57 @@ import { User } from 'src/app/models/User.model';
   styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent implements OnInit {
-  // EditButtonClicked: boolean = false;
+  EditButtonClicked: boolean;
+  loggedUser: User;
 
-  // location: string =
-  //   this.userService.getaUserProperty(
-  //     this.authService.getLoggedUserId(),
-  //     'location'
-  //   ) || '';
-  // hometown: string =
-  //   this.userService.getaUserProperty(
-  //     this.authService.getLoggedUserId(),
-  //     'hometown'
-  //   ) || '';
-  // work_in: string =
-  //   this.userService.getaUserProperty(
-  //     this.authService.getLoggedUserId(),
-  //     'work_in'
-  //   ) || '';
-  // birthdayObject: {
-  //   year: string;
-  //   month: string;
-  //   day: string;
-  // } = this.userService.getaUserProperty(
-  //   this.authService.getLoggedUserId(),
-  //   'birthday'
-  // );
-  // birthday: string = this.birthdayObject.year
-  //   ? `${this.birthdayObject.year}/${this.birthdayObject.month}/${this.birthdayObject.day}`
-  //   : '';
+  location: string;
+  hometown: string;
+  work_in: string;
+  birthdayObject: Birthday;
 
   constructor(
     private userService: UserServices,
     private authService: AuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.EditButtonClicked = false;
+    this.authService.getAuthenticatedUser().then((user: User) => {
+      if (user) {
+        this.loggedUser = user;
+        user.location ? (this.location = user.location) : (this.location = ' ');
+        user.hometown ? (this.hometown = user.hometown) : (this.hometown = ' ');
+        user.workIn ? (this.work_in = user.workIn) : (this.work_in = ' ');
+        user.birthday
+          ? (this.birthdayObject = user.birthday)
+          : (this.birthdayObject = null);
+      } else {
+      }
+    });
+  }
 
   showInputs = () => {
-    // this.EditButtonClicked = true;
+    this.EditButtonClicked = true;
   };
-  // editUserInfos = (f: NgForm) => {
-  //   const location = f.value.location;
-  //   const hometown = f.value.hometown;
-  //   const work_in = f.value.work_in;
-  //   const birthday = f.value.birthday;
+  editUserInfos = (f: NgForm) => {
+    const location = f.value.location;
+    const hometown = f.value.hometown;
+    const work_in = f.value.work_in;
+    const birthday = f.value.birthday;
 
-  //   this.location = location;
-  //   this.hometown = hometown;
-  //   this.work_in = work_in;
-  //   this.birthday = birthday;
+    this.location = location;
+    this.hometown = hometown;
+    this.work_in = work_in;
+    this.birthdayObject = birthday;
 
-  //   this.userService.updateUser(
-  //     this.authService.getLoggedUserId(),
-  //     'location',
-  //     location
-  //   );
-  //   this.userService.updateUser(
-  //     this.authService.getLoggedUserId(),
-  //     'hometown',
-  //     hometown
-  //   );
-  //   this.userService.updateUser(
-  //     this.authService.getLoggedUserId(),
-  //     'work_in',
-  //     work_in
-  //   );
-  //   this.userService.updateUser(
-  //     this.authService.getLoggedUserId(),
-  //     'birthday',
-  //     birthday
-  //   );
+    this.loggedUser.location = location;
+    this.loggedUser.hometown = hometown;
+    this.loggedUser.workIn = work_in;
+    this.loggedUser.birthday = birthday;
 
-  //   this.EditButtonClicked = false;
-  // };
+    this.userService
+      .updateUser(this.loggedUser)
+      .subscribe((result) => console.log(result));
+    this.EditButtonClicked = false;
+  };
 }
