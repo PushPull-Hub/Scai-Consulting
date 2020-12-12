2c0f9de4243ca267c0b35a9d8df7a87a49a6acc7
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-12 16:49:58 +0100

    add a rest controller to test after deploy

--- back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/controllers/TestController.java
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/controllers/TestController.java
@@ -2,12 +2,15 @@ package com.scaiconsulting.scaichat.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@Controller
+@RestController
+@RequestMapping("/")
 public class TestController {
-    @GetMapping("/")
+    @GetMapping("")
     public String confirmFunctionalityOfTheApp () {
         return "The Application is running correctly, time on server is : " + LocalDateTime.now() ;
     }
