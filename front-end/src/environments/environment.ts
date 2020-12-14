1a3fb0ebee7a81eb1fe19012e481b4bbd3895a3d
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-14 09:57:42 +0100

    add the provided url by heroku after deploy be

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  rootUrl: 'http://localhost:8080',
+  rootUrl: 'https://morning-everglades-15984.herokuapp.com',
   male_avatar_photo_url: './assets/images/male-user.jpg',
   female_avatr_photo_url: './assets/images/female-user.jpg',
 };
