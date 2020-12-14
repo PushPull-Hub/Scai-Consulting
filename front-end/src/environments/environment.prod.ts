92c11eefdf6caccf1ebfa19cd096b41f8e4683a3
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-14 11:31:17 +0100

    fix typos in the url and add mysql dialect to app prop

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  rootUrl: 'https://morning-everglades-15984.herokuapp.com/',
+  rootUrl: 'https://morning-everglades-15984.herokuapp.com',
   male_avatar_photo_url: './assets/images/male-user.jpg',
   female_avatr_photo_url: './assets/images/female-user.jpg',
 };
