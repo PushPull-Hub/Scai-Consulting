a5c6a4619227d6e5ed4bc3e078adb9dff17c721b
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-15 12:31:42 +0100

    solve cors issue by allow all headers

--- back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/controllers/TestController.java
+++ back-end/scai-chat/src/main/java/com/scaiconsulting/scaichat/controllers/TestController.java
@@ -1,5 +1,6 @@
 package com.scaiconsulting.scaichat.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = "Authentication")
 public class TestController {
     @GetMapping("")
     public String confirmFunctionalityOfTheApp () {
