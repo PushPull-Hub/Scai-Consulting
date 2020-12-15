cb825d8b93e2aba2d3c65dc5933b8c195c3ab7d6
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-15 17:29:29 +0100

    refactor the friend suggestions component logic

--- front-end/src/app/components/home-wrapper/home-page/friendships-suggestion/friendships-suggestion.component.ts
+++ front-end/src/app/components/home-wrapper/home-page/friendships-suggestion/friendships-suggestion.component.ts
@@ -3,6 +3,7 @@ import { FriendsService } from 'src/app/services/friends.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserServices } from 'src/app/services/user.service';
 import { environment } from 'src/environments/environment';
+import { MiniProfile } from 'src/app/models/MiniProfile.model';
 
 @Component({
   selector: 'app-friendships-suggestion',
@@ -10,9 +11,11 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./friendships-suggestion.component.scss'],
 })
 export class FriendshipsSuggestionComponent implements OnInit {
-  friendsSuggestion: string[];
-  clicked = false;
+  friendsSuggestion: MiniProfile[];
+  profiles: MiniProfile[];
+  loading: boolean;
   male_avatar_photo_url: string;
+
   constructor(
     private friendsService: FriendsService,
     private authService: AuthService,
@@ -20,10 +23,42 @@ export class FriendshipsSuggestionComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // this.friendsSuggestion = this.friendsService
-    //   .getTenFriendsSuggestion()
-    //   .slice(0, 5);
-    // this.male_avatar_photo_url = environment.male_avatar_photo_url;
+    this.male_avatar_photo_url = environment.male_avatar_photo_url;
+    this.loadProfiles();
+  }
+
+  private loadProfiles() {
+    this.loading = true;
+    this.friendsService
+      .getTenSuggestions()
+      .toPromise()
+      .then((res) => {
+        if (res && res.length > 0) {
+          this.friendsSuggestion = res;
+          this.profiles = res.slice(0, 5);
+          this.loading = false;
+        }
+      })
+      .catch((err) => {
+        this.loading = false;
+        console.log(err);
+      });
+  }
+
+  sendFriendRequest(requested_user_Id: number) {
+    this.friendsService
+      .sendFriendRequest(requested_user_Id)
+      .toPromise()
+      .then((res) => {
+        if (res && res.id) {
+          this.profiles = this.profiles.filter(
+            (suggested) => suggested.id !== requested_user_Id
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   // getFriendProperty = (id: string, property: string) =>
