6b6e3f216686136719b913c9c0bfddeb311092ab
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-15 11:57:48 +0100

    change the url to point to the new deployed back-end

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  rootUrl: 'https://morning-everglades-15984.herokuapp.com',
+  rootUrl: ' https://glacial-badlands-58060.herokuapp.com',
   male_avatar_photo_url: './assets/images/male-user.jpg',
   female_avatr_photo_url: './assets/images/female-user.jpg',
 };
