a149265a7d43bc9784c6643b6d6cde59edb3eb42
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-16 09:16:09 +0100

    add logic to show an error message in case of fail

--- front-end/src/app/components/home-wrapper/home-page/friendships-suggestion/friendships-suggestion.component.ts
+++ front-end/src/app/components/home-wrapper/home-page/friendships-suggestion/friendships-suggestion.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FriendsService } from 'src/app/services/friends.service';
-import { AuthService } from 'src/app/services/auth.service';
-import { UserServices } from 'src/app/services/user.service';
 import { environment } from 'src/environments/environment';
 import { MiniProfile } from 'src/app/models/MiniProfile.model';
 
@@ -15,15 +13,14 @@ export class FriendshipsSuggestionComponent implements OnInit {
   profiles: MiniProfile[];
   loading: boolean;
   male_avatar_photo_url: string;
+  errorOnLoadingProfiles: boolean;
+  noProfilesToLoad: boolean;
 
-  constructor(
-    private friendsService: FriendsService,
-    private authService: AuthService,
-    private userService: UserServices
-  ) {}
+  constructor(private friendsService: FriendsService) {}
 
   ngOnInit(): void {
     this.male_avatar_photo_url = environment.male_avatar_photo_url;
+    this.errorOnLoadingProfiles = false;
     this.loadProfiles();
   }
 
@@ -35,12 +32,18 @@ export class FriendshipsSuggestionComponent implements OnInit {
       .then((res) => {
         if (res && res.length > 0) {
           this.friendsSuggestion = res;
-          this.profiles = res.slice(0, 5);
+          this.profiles = this.friendsSuggestion.slice(0, 5);
           this.loading = false;
+        } else {
+          this.loading = false;
+          this.errorOnLoadingProfiles = false;
+          this.profiles = [];
         }
+        console.log(this.friendsSuggestion);
       })
       .catch((err) => {
         this.loading = false;
+        this.errorOnLoadingProfiles = true;
         console.log(err);
       });
   }
@@ -51,25 +54,17 @@ export class FriendshipsSuggestionComponent implements OnInit {
       .toPromise()
       .then((res) => {
         if (res && res.id) {
-          this.profiles = this.profiles.filter(
-            (suggested) => suggested.id !== requested_user_Id
-          );
+          if (this.profiles.length > 0) {
+            this.profiles = this.profiles.filter(
+              (profile) => profile.id !== requested_user_Id
+            );
+          } else if (this.profiles.length == 0) {
+            this.loadProfiles();
+          }
         }
       })
       .catch((err) => {
         console.log(err);
       });
   }
-
-  // getFriendProperty = (id: string, property: string) =>
-  //   this.userService.getaUserProperty(id, property);
-
-  // addFriend(addedId) {
-  //   this.friendsService.addFriend(addedId);
-  //   setTimeout(() => {
-  //     this.friendsSuggestion = this.friendsSuggestion.filter(
-  //       (suggested) => suggested !== addedId
-  //     );
-  //   }, 500);
-  // }
 }
