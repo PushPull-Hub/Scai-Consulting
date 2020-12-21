ba5bc2c408394f668616960464892b5101aba5ff
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-21 17:54:01 +0100

    create profile for other usesrs

--- front-end/src/app/shared/friend-profile/friend-profile.component.ts
+++ front-end/src/app/shared/friend-profile/friend-profile.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MiniProfile } from 'src/app/models/MiniProfile.model';
+import { Post } from 'src/app/models/Post.model';
+import { User } from 'src/app/models/User.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-friend-profile',
   templateUrl: './friend-profile.component.html',
-  styleUrls: ['./friend-profile.component.scss']
+  styleUrls: ['./friend-profile.component.scss'],
 })
 export class FriendProfileComponent implements OnInit {
+  @Input() user: User;
+  userPosts: Post[];
+  male_avatar_photo_url: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.male_avatar_photo_url = environment.male_avatar_photo_url;
   }
-
 }
