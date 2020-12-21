b4e0d835a4bf7835d6b4bea1136a6122f01e3939
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-21 17:55:49 +0100

    remove component from this model

--- front-end/src/app/components/home-wrapper/profile/second-wrapper/timeline/timeline.module.ts
+++ front-end/src/app/components/home-wrapper/profile/second-wrapper/timeline/timeline.module.ts
@@ -3,25 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TimelineRoutingModule } from './timeline-routing.module';
 
-import { IntroComponent } from './intro/intro.component';
 import { TimelineCreatePostComponent } from './timeline-create-post/timeline-create-post.component';
 import { PhotosComponent } from './photos/photos.component';
-import { TimelinePostsComponent } from './timeline-posts/timeline-posts.component';
 import { SharedModule } from '../../../../../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    IntroComponent,
-    TimelineCreatePostComponent,
-    PhotosComponent,
-    TimelinePostsComponent,
-  ],
+  declarations: [TimelineCreatePostComponent, PhotosComponent],
   imports: [CommonModule, TimelineRoutingModule, SharedModule],
-  exports: [
-    IntroComponent,
-    TimelineCreatePostComponent,
-    PhotosComponent,
-    TimelinePostsComponent,
-  ],
+  exports: [TimelineCreatePostComponent, PhotosComponent],
 })
 export class TimelineModule {}
