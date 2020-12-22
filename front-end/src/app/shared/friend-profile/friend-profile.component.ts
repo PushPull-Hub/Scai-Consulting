2d30f6f5674db20e0176b8c6acd3e26f5a6a13e4
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-22 20:57:31 +0100

    add required method to manage relationships

--- front-end/src/app/shared/friend-profile/friend-profile.component.ts
+++ front-end/src/app/shared/friend-profile/friend-profile.component.ts
@@ -3,9 +3,6 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FriendShip } from 'src/app/models/FriendShip.model';
 import { MiniProfile } from 'src/app/models/MiniProfile.model';
 import { Post } from 'src/app/models/Post.model';
-import { PostComment } from 'src/app/models/PostComment.model';
-import { PostLike } from 'src/app/models/PostLike.model';
-import { User } from 'src/app/models/User.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { FriendsService } from 'src/app/services/friends.service';
 import { PostsService } from 'src/app/services/posts.service';
@@ -78,9 +75,6 @@ export class FriendProfileComponent implements OnInit {
         .getRelation(this.id)
         .toPromise()
         .then((result: FriendShip) => {
-          console.log(result);
-          console.log(this.id);
-
           if (result) {
             if (result.status == 0 && result.actionUserId !== this.id) {
               this.relationShipStatus = 'a';
@@ -110,69 +104,95 @@ export class FriendProfileComponent implements OnInit {
     });
   }
 
+  sendFriendsRequest(userId: number) {
+    this.friendService
+      .sendFriendRequest(userId)
+      .toPromise()
+      .then((result) => {
+        if (result) {
+          console.log(result);
+          this.relationShipStatus = 'a';
+        } else console.log('check conditions ');
+      });
+  }
+
+  acceptFriendRequest(requestorId: number) {
+    this.friendService
+      .acceptFriendRequest(requestorId)
+      .subscribe((result: FriendShip) => {
+        if (result) {
+          console.log(result);
+          this.relationShipStatus = 'c';
+        } else console.log('check conditions ');
+      });
+  }
+
+  declineFriendRequest(requestorId: number) {
+    this.friendService.declineFriendRequest(requestorId).subscribe((result) => {
+      if (result) {
+        console.log(result);
+        this.relationShipStatus = 'f';
+      } else console.log('check the conditions ');
+    });
+  }
+
+  cancelFriendRequest(requestedUserId) {
+    this.friendService
+      .cancelFriendRequest(requestedUserId)
+      .subscribe((result: boolean) => {
+        if (result) {
+          console.log(result);
+          this.relationShipStatus = 'f';
+        } else console.log('check conditions ');
+      });
+  }
+
+  blockFriend(friendId: number) {
+    this.friendService
+      .blockFriend(friendId)
+      .toPromise()
+      .then((result) => {
+        console.log(result);
+        if (result) {
+          this.relationShipStatus = 'e';
+        } else console.log('check conditions ');
+      });
+  }
+
+  unblockFriend(friendId: number) {
+    this.friendService
+      .unblockFriend(friendId)
+      .toPromise()
+      .then((result) => {
+        console.log(result);
+        if (result) {
+          this.relationShipStatus = 'c';
+        } else console.log('check conditions ');
+      });
+  }
+
+  unFriend(friendId: number) {
+    this.friendService
+      .unFriend(friendId)
+      .toPromise()
+      .then((result) => {
+        if (result) {
+          console.log(result);
+          this.relationShipStatus = 'f';
+        } else console.log('check conditions ');
+      });
+  }
+
   // enum Status {
-  //   Pending = 0,          a I sent / b he sent  , c friends , d he blocked , e i blocked , f no relation
+  //   Pending = 0,
   //   Accepted = 1,
   //   Declined = 2,
   //   Blocked = 3,
   // }
 
-  // private loadPosts() {
-  //   this.postService.getFriendsPosts().subscribe((posts) => {
-  //     this.doIhavePostsToShow = true;
-  //     setTimeout(() => {
-  //       if (posts && posts.length > 0) {
-  //         this.posts = posts;
-  //         this.sortPostsByDate();
-  //         this.doIhavePostsToShow = true;
-  //         this.loading = false;
-  //       } else {
-  //         this.loading = false;
-  //         this.posts = [];
-  //         this.doIhavePostsToShow = false;
-  //       }
-  //     }, 600);
-  //   });
-  // }
-
-  // addCommentOnPost(comment: PostComment) {
-  //   const index = this.posts.findIndex((post) => post.id == comment.postId);
-  //   if (index != -1) {
-  //     this.postService.commentOnPost(comment).subscribe((responseData) => {
-  //       if (responseData.id) this.posts[index].comments.push(responseData);
-  //     });
-  //   }
-  // }
-
-  // likePost(id: number) {
-  //   const index = this.posts.findIndex((post) => post.id == id);
-  //   if (index != -1) {
-  //     this.postService
-  //       .likePost(id)
-  //       .subscribe(
-  //         (likerIds: PostLike[]) => (this.posts[index].likerIds = likerIds)
-  //       );
-  //   } else {
-  //     console.log('check conditions  ');
-  //   }
-  // }
-
-  // unlikePost(id: number) {
-  //   const index = this.posts.findIndex((post) => post.id == id);
-  //   if (index != -1) {
-  //     this.postService
-  //       .unlikePost(id)
-  //       .subscribe(
-  //         (likerIds: PostLike[]) => (this.posts[index].likerIds = likerIds)
-  //       );
-  //   } else {
-  //     console.log('check conditions  ');
-  //   }
-  // }
-
-  // sortPostsByDate() {
-  //   return (this.sortedPosts = this.posts.sort(
-  //     (a, b) => parseInt(b.created_time) - parseInt(a.created_time)
-  //   ));
-  // }
+  // a - I sent
+  // c - we are friends
+  // d - he blocked
+  // e - I  blocked
+  // f - no relation
 }
