7d108686e486bd28f5542c5fb067be53958abd2e
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-23 09:46:45 +0100

    show active friends on the box

--- front-end/src/app/components/home-wrapper/home-page/active-friends/active-friends.component.ts
+++ front-end/src/app/components/home-wrapper/home-page/active-friends/active-friends.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FriendsService } from '../../../../services/friends.service';
 import { environment } from 'src/environments/environment';
+import { MiniProfile } from 'src/app/models/MiniProfile.model';
 
 @Component({
   selector: 'app-active-friends',
@@ -11,19 +12,21 @@ export class ActiveFriendsComponent implements OnInit {
   clicked: boolean = false;
   // ActiveFriends: Friend[];
   male_avatar_photo_url: string;
+  myFriendsProfiles: MiniProfile[];
 
   constructor(private friendsService: FriendsService) {}
 
   ngOnInit(): void {
     this.male_avatar_photo_url = environment.male_avatar_photo_url;
+    this.friendsService
+      ._getMyFriendsProfiles(this.friendsService.myFriends)
+      .then((result) => {
+        this.myFriendsProfiles = result;
+      });
   }
 
   showActiveFriends = () => {
-    //  this.clicked = !this.clicked;
+    this.clicked = !this.clicked;
     //this.ActiveFriends = this.friendsService.getActiveFriendsList();
   };
-
-  showFriendUserName(id: string) {
-    //return this.friendsService.getaFriendProperty(id, 'username');
-  }
 }
