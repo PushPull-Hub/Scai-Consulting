510f76891cb6400a4053ed750c06caa85c7465e5
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-23 17:02:52 +0100

    add all require modules for running firebase on app module

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { AuthentictionModule } from './components/auth-wrapper/authentication.module';
+
 import { HomeWrapperModule } from './components/home-wrapper/home-wrapper.module';
 import { SharedModule } from './shared/shared.module';
+
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth-interceptor.ts.interceptor';
+
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +27,9 @@ import { AuthInterceptor } from './interceptors/auth-interceptor.ts.interceptor'
     AuthentictionModule,
     HomeWrapperModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
   providers: [
     {
