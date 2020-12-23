caf496a69336ffe02951aba5c04bc1d5ee09fd96
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-12-23 17:03:17 +0100

    add firebase configuration to the env file

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -4,4 +4,14 @@ export const environment = {
   //rootUrl: 'http://localhost:8080',
   male_avatar_photo_url: './assets/images/male-user.jpg',
   female_avatr_photo_url: './assets/images/female-user.jpg',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBmg9hoogcHN4ZER6CsSzlKkKL2FPJVRgU',
+    authDomain: 'scai-chat-images.firebaseapp.com',
+    databaseURL: 'https://scai-chat-images-default-rtdb.firebaseio.com',
+    projectId: 'scai-chat-images',
+    storageBucket: 'scai-chat-images.appspot.com',
+    messagingSenderId: '810681715218',
+    appId: '1:810681715218:web:f896fa1e0e9a2f1b1be6cb',
+    measurementId: 'G-H7RM3V2E3B',
+  },
 };
