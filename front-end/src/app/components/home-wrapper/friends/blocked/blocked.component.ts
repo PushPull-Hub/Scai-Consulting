7bf6ce751109ea5fb48b820354434bbe90a9480c
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-11 17:00:14 +0100

    render the empty user's profile picture up to his gender

--- front-end/src/app/components/home-wrapper/friends/blocked/blocked.component.ts
+++ front-end/src/app/components/home-wrapper/friends/blocked/blocked.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Gender } from 'src/app/models/Gender.model';
 import { MiniProfile } from 'src/app/models/MiniProfile.model';
 import { FriendsService } from 'src/app/services/friends.service';
 import { environment } from 'src/environments/environment';
@@ -9,16 +10,22 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./blocked.component.scss'],
 })
 export class BlockedComponent implements OnInit {
-  male_avatar_photo_url: string;
-  loading: boolean = true;
-  BlockedByMeListProfiles: MiniProfile[];
-  doIhaveBlockedUsersByMe: boolean = true;
   @Output() friendShipEmitter: EventEmitter<any> = new EventEmitter();
+  BlockedByMeListProfiles: MiniProfile[];
+
+  male_avatar_photo_url: string;
+  female_avatar_photo_url: string;
+  Female;
+
+  loading: boolean = true;
+  doIhaveBlockedUsersByMe: boolean = true;
 
   constructor(private friendService: FriendsService) {}
 
   ngOnInit(): void {
     this.male_avatar_photo_url = environment.male_avatar_photo_url;
+    this.female_avatar_photo_url = environment.female_avatr_photo_url;
+    this.Female = Gender[1];
   }
 
   loadProfiles(data) {
